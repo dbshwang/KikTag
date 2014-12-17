15431fda44d26e6f0c21790fe9283b7a938ad2a3
Sanchit Gera <sanchit.gera@kik.com>
2014-12-16 19:54:46 -0500

    mongo ftw

--- starter-kit/web/zerver/API.js
+++ starter-kit/web/zerver/API.js
@@ -1,6 +1,53 @@
 exports.create = create;
 function create(params, callback) {
-console.log('YOU ARE IN CREATE!!!');   
-console.log(params.data);
-  callback('ok');
+  var doc = init_doc(params.data);
+
+  connectToDB(function(db){
+    collection = db.collection('submitted_hashtags');
+
+   collection.insert(doc, function(err, objects) {
+     console.log(err);
+      if (err) callback('Error');
+      else callback('ok');
+      db.close();
+    });
+
+  });
+}
+
+view.type = 'GET';
+exports.view = view;
+function view(params, callback){
+  connectToDB( function(db) {
+    collection = db.collection('submitted_hashtags');
+    collection.find().toArray(function(err, docs){
+       console.log(err);
+       docs.forEach(function(doc){
+         delete doc['_id'];
+       });
+       callback(docs);
+       db.close();
+    });
+  });
+}
+
+function connectToDB(callback){
+  var MongoClient = require('mongodb').MongoClient
+  , format  = require('util').format;
+  collection = {};
+  MongoClient.connect('mongodb://127.0.0.1:27017/HASHTAGS', function(err, db) {
+    if(err) throw err;
+    callback(db);
+  });
+}
+
+function init_doc(data){
+  doc = {};
+  doc['hashtag']  = data['hashtag'];
+  doc['user']     = data['user'];
+  doc['category'] = data['category'];
+  doc['rating']   = 0;
+  doc['date']     = (new Date().getTime() / 1000);
+  doc['desc']     = data['desc'];
+  return doc;
 }
