8504fbb7af40b10f843357131828ef7c2f1f4e11
Daniel Hwang <daniel.hwang11@gmail.com>
2014-12-16 20:12:58 -0500

    merge server changes

--- starter-kit/web/controller/search_group.js
+++ starter-kit/web/controller/search_group.js
@@ -2,8 +2,10 @@ App.controller('search_group', function (page) {
     page.querySelector('.submit').addEventListener('click', function(){
         App.load('kik_group', 'slideon-down');
     });
-
     zerver.get('API/view', function(response){
       console.log(response);
+        
+    page.querySelector('.list').addEventListener('click', function(){
+        $("#scrollbar").toggle();
     });
 });
