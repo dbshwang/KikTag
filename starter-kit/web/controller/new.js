b1afc831531c440fc746a22b0721b60fb8eef8d6
Daniel Hwang <daniel.hwang11@gmail.com>
2014-12-16 22:35:21 -0500

    cleanup search group js

---
+++ starter-kit/web/controller/new.js
@@ -0,0 +1,20 @@
+App.controller('new', function (page) {
+    
+    function callback(str){
+      // console.log(str);
+      $('.supercontainer').append(str);
+    }
+
+    $(page).on('appReady', function(){
+      zerver.get('API/view', function(hashtags){
+        super_el = $('<div class="groupcontainer"></div>');
+        for(var i = 0; i< hashtags.length; i++){
+          el = $('<div class="populate_group"><div class="info"></div></div>');
+          el.find('.info').append('<h3>' + hashtags[i]['hashtag'] + '</h3>');
+          el.find('.info').append('<p>' + hashtags[i]['desc'] + '</p>');
+          super_el.append(el);
+        }
+        callback(super_el);
+      });
+    });
+});
--- starter-kit/web/controller/search_group.js
+++ starter-kit/web/controller/search_group.js
@@ -1,28 +1,5 @@
 App.controller('search_group', function (page) {
-
     page.querySelector('.submit').addEventListener('click', function(){
         App.load('kik_group', 'slideon-down');
-    });
-
-    function callback(str){
-      // console.log(str);
-      $('.supercontainer').append(str);
-    }
-
-    $(page).on('appShow', function(){
-      zerver.get('API/view', function(hashtags){
-        super_el = $('<div class="groupcontainer"></div>');
-        for(var i = 0; i< hashtags.length; i++){
-          el = $('<div class="populate_group"><div class="info"></div></div>');
-          el.find('.info').append('<h3>' + hashtags[i]['hashtag'] + '</h3>');
-          el.find('.info').append('<p>' + hashtags[i]['desc'] + '</p>');
-          super_el.append(el);
-        }
-        callback(super_el);
-      });
-    });
-
-    page.querySelector('.list').addEventListener('click', function(){
-        $("#scrollbar").toggle();
     });
 });
