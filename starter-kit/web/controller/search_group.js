e74de6adf07d5fbc9690c7e8ec89e3dd4870254c
Sanchit Gera <sanchit.gera@kik.com>
2014-12-16 21:55:15 -0500

    populate the data dynamically

--- starter-kit/web/controller/search_group.js
+++ starter-kit/web/controller/search_group.js
@@ -1,9 +1,26 @@
 App.controller('search_group', function (page) {
+
     page.querySelector('.submit').addEventListener('click', function(){
         App.load('kik_group', 'slideon-down');
     });
 
-    zerver.get('API/view', function(response){
-      console.log(response);
+    function callback(str){
+      // console.log(str);
+      $('.supercontainer').append(str);
+    }
+
+    $(page).on('appShow', function(){
+
+      zerver.get('API/view', function(hashtags){
+        super_el = $('<div class="groupcontainer"></div>');
+        for(var i = 0; i< hashtags.length; i++){
+          el = $('<div class="populate_group"><div class="info"></div></div>');
+          el.find('.info').append('<h3>' + hashtags[i]['hashtag'] + '</h3>');
+          el.find('.info').append('<p>' + hashtags[i]['desc'] + '</p>');
+          super_el.append(el);
+        }
+        callback(super_el);
+      });
     });
+
 });
